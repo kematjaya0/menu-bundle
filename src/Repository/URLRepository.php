62d9c6377fa5bdb6de2fd3c53d7369864e25d1ff
Nur Hidayatullah <kematjaya0@gmail.com>
2021-07-20 21:45:57 +0700

    update menu access control

--- src/Repository/URLRepository.php
+++ src/Repository/URLRepository.php
@@ -32,13 +32,44 @@ class URLRepository extends BaseRepository
     public function findAll(string $role):array
     {
         $routers = parent::findAll($role);
-        dump($this->menuBuilder);
-        dump($routers);exit;
+        foreach ($this->getMenuWithRoles() as $routeName => $value) {
+            $key = str_replace('_index', '', $routeName);
+            if (!isset($routers[$key])) {
+                continue;
+            }
+            
+            $routers[$key][$routeName] = in_array($role, $value['role']);
+        }
+        
+        return $routers;
     }
     
     public function save(array $routers): void 
     {
-        dump($routers);exit;
+        $menus = $this->menuBuilder->getMenus();
+        foreach ($menus as $routeName => $value) {
+            if (!isset($routers[$routeName])) {
+                continue;
+            }
+            
+            if (!isset($value['role'])) {
+                continue;
+            }
+            
+            $menus[$routeName]['role'] = $routers[$routeName];
+        }
+        
+        $this->menuBuilder->dump($menus);
+        
         parent::save($routers);
+    }
+    
+    protected function getMenuWithRoles():array
+    {
+        $menus = $this->menuBuilder->getMenus();
+        return array_filter($menus, function ($menu){
+            
+            return isset($menu['role']);
+        });
     }
 }
